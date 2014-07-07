bcce9c855cb0b80f9f792f19e02b6c80698733bf
Koji <afterjnih@gmail.com>
2014-07-08 00:47:06 +0900

    a

--- piggybank.rb
+++ piggybank.rb
@@ -2,6 +2,6 @@ require 'rack'
 
 class Piggybank
   def call(env)
-    [200, {"Content-Type" => "text/plain"},"Hello piggybank"]
+    [200, {"Content-Type" => "text/plain"},["Hello piggybank"]]
   end
 end
