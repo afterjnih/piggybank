a02e598d8201aebc2e2b09a2113e70f0c3a382be
Koji <afterjnih@gmail.com>
2014-07-12 15:01:52 +0900

    a

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
+load 'show.rb'
 
 get '/' do
   'Hello World!'
 end
 
 get '/about' do
-  'About This Site3'
+  sim = ShowInputMenu.new()
+  sim.show
+ # 'About This Site3'
 end
